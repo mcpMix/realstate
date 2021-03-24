a317e89ebfd8a45536bd41dcb9635130be32561a
Cristiano Braga <cribraga@gmal>
2021-03-24 15:17:56 -0300

    Inicio projeto administração de imobiliaria

---
+++ src/test/java/br/com/realstate/RealstateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.realstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
